fa2f2572b8a1c6c218cb1fb0c8f12e4d0449fb48
Timur <timur.idalov@stud.fh-campuswien.ac.at>
2023-03-23 15:38:47 +0100

    Adjusted client function call

--- client/client.py
+++ client/client.py
@@ -1,5 +1,5 @@
 from zeep import Client
 
 client = Client('http://127.0.0.1:8080/?wsdl')
-result = client.service.convert('JPY', 'CHF', '500')
+result = client.service.convert('apikey1', 'JPY', 'CHF', '500')
 print("500 JPY in CHF is " + str(result))
